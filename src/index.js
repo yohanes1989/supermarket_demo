ef56325b256844f9434781948c6ba31cbf915a69
Yohanes <yohanes@webpresso.co.id>
2017-07-10 05:17:56 +0700

    change router to hash instead

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import App from './Component/App';
 import CatalogEdit from './Component/CatalogEdit';
@@ -11,12 +11,12 @@ import reducers from './reducers';
 
 const AppWithProvider = (
   <Provider store={ createStore(reducers, {}, applyMiddleware(reduxThunk)) }>
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/catalog-form" component={CatalogEdit} />
         <Route path="/" component={App} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
